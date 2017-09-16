9f3b311c0bc150e63ae68b830a25bb707c45899d
laplandin <deniskakhv@gmail.com>
2017-09-16 20:32:26 +0300

    responsive repertoire-item fix

--- src/partials/components/repertoire-item/repertoire-item.js
+++ src/partials/components/repertoire-item/repertoire-item.js
@@ -1,9 +1,13 @@
 (function () {
-    $(window).on('load', function() {
-        var parentWidth = $('.repertoire-item').width()/2;
-        var h = Math.sin(0.087) * parentWidth
-        $('.repertoire-item:first-child').css('marginTop', -h/4);
-        $('.repertoire-item__info').css({paddingTop: h*1.5, top: -h/1.8, bottom: -h/1.8});
+    $(window).on('load resize', function() {
+       calcItem();
     });
+
+    function calcItem () {
+        var parentWidth = $('.repertoire-item').width()/2;
+        var h = Math.sin(0.087) * parentWidth;
+        $('.repertoire-item:first-child').css('marginTop', -h/4);
+        $('.repertoire-item__info').css({paddingTop: h, top: -h/1.8, bottom: -h/1.8});
+    }
 }());
 
